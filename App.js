fbeabf136ca03b8e6a0bfa287b4aac520006f711
andreanavas18 <karennavas333@gmail.com>
2024-09-07 22:47:49 -0500

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+
+const icon = require('./assets/ti.png');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Image source={icon} style={{ width: 100, height: 100 }} /> 
+      <Text style={{color: 'white'}}>Welcome to Digital Cards</Text>
+      <Button title="Pulsa aqui" onPress={() => alert('Hola')} />
     </View>
   );
 }
@@ -13,7 +17,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#599b9b',
     alignItems: 'center',
     justifyContent: 'center',
   },
